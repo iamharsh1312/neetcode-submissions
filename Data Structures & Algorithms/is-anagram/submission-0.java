dc6feef6c77ab6f34f6301918de044d9235093b0
Harsh <65723273+iamharsh1312@users.noreply.github.com>
2026-08-10 21:13:04 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int n=s.length();
+        int cnt[] = new int[26];
+       
+        for (int i = 0; i < s.length(); i++) {
+            cnt[s.charAt(i)-'a']++;
+            cnt[t.charAt(i)-'a']--;
+        }
+        for(int val:cnt){
+            if(val!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
